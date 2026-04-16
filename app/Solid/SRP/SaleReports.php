386218a81115b2b7a99fda3318cb0320be8c8331
Malka Anjum <75417185+MALKA-ANJUM@users.noreply.github.com>
2026-04-16 22:56:46 +0530

    SaleReports updated

--- app/Solid/SRP/SaleReports.php
+++ app/Solid/SRP/SaleReports.php
@@ -8,6 +8,9 @@ class SaleReports
 {
     public function between($startDate, $endDate)
     {
-        return DB::table('sales')->whereBetween('created_at', [$startDate, $endDate])->latest()->get();
+        return DB::table('sales')
+            ->whereBetween('created_at', [$startDate, $endDate])
+            ->latest()
+            ->get();
     }
 }
